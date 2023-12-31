3252d62543e052f6cf4f0ada4d03cf93a5bf59cc
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-12-31 13:35:36 -0800

    Add files via upload

---
+++ play.js
@@ -0,0 +1,17 @@
+chrome.contextMenus.create({
+    id:"GuitarMan",
+    title:"GuitarMan",
+    contexts: ["selection"]
+})
+
+chrome .contextMenus.onClicked.addListener(function (info, tab) {
+    if(info.menuItemId){
+        //`${info.menuItemId} ${variableName}`
+        //const variableName = info.selectionText;
+        //chrome.tabs.create({ url: `$variableName}` });
+        const tellme = info.selectionText;
+        chrome.tabs.create({ url: `https://www.ultimate-guitar.com/search.php?search_type=title&value=${tellme}` });
+        chrome.tabs.create({ url: `https://www.youtube.com/results?search_query=${tellme}` });
+        chrome.tabs.create({ url: `https://www.spotify.com/search/${tellme}` });
+    }   
+});
